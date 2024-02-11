541e7335c44ddb4d8ba93db52658c1ca615cf21d
Alex <ascott.sco@gmail.com>
2024-02-11 13:47:09 +0000

    Configured lspconfig, and mason

--- nvim/lua/plugins/telescope.lua
+++ nvim/lua/plugins/telescope.lua
@@ -1,13 +1,31 @@
 return {
-  "nvim-telescope/telescope.nvim",
-  tag = "0.1.5",
-  dependencies = {
-    "nvim-lua/plenary.nvim"
+  {
+    "nvim-telescope/telescope.nvim",
+    tag = "0.1.5",
+    dependencies = {
+      "nvim-lua/plenary.nvim"
+    },
+    config = function()
+      local builtin = require("telescope.builtin")
+      vim.keymap.set("n", "<leader>ff", builtin.find_files, {})
+      vim.keymap.set("n", "<leader>fg", builtin.live_grep, {})
+      vim.keymap.set("n", "<leader>fz", builtin.current_buffer_fuzzy_find, {})
+    end
   },
-  config = function()
-    local builtin = require("telescope.builtin")
-    vim.keymap.set("n", "<leader>ff", builtin.find_files, {})
-    vim.keymap.set("n", "<leader>fg", builtin.live_grep, {})
-    vim.keymap.set("n", "<leader>fz", builtin.current_buffer_fuzzy_find, {})
-  end
+  {
+    "nvim-telescope/telescope-ui-select.nvim",
+    config = function()
+      require("telescope").setup({
+        extension = {
+          ["ui-select"] = {
+            require("telescope.themes").get_dropdown {
+              --opts
+            }
+          }
+        }
+      })
+
+      require("telescope").load_extension("ui-select")
+    end
+  }
 }
