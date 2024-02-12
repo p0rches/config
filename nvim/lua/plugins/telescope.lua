89bdd962a71146fd12914f95ada23956500b38e3
Alex <ascott.sco@gmail.com>
2024-02-12 22:24:47 +0000

    Added telescope keymap for TreeSitter highlights

--- nvim/lua/plugins/telescope.lua
+++ nvim/lua/plugins/telescope.lua
@@ -10,6 +10,7 @@ return {
       vim.keymap.set("n", "<leader>ff", builtin.find_files, {})
       vim.keymap.set("n", "<leader>fg", builtin.live_grep, {})
       vim.keymap.set("n", "<leader>fz", builtin.current_buffer_fuzzy_find, {})
+      vim.keymap.set("n", "<leader>fh", builtin.highlights, {})
     end
   },
   {
