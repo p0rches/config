3c102807731be0d57e6bdf465a1935c2f2ea3266
Alex <ascott.sco@gmail.com>
2024-02-15 20:37:17 +0000

    Nvim plugin configuration and tmux config

--- nvim/lua/plugins/lualine.lua
+++ nvim/lua/plugins/lualine.lua
@@ -1,13 +1,15 @@
 return {
   "nvim-lualine/lualine.nvim",
   dependencies = {
-    "nvim-tree/nvim-web-devicons"
+    "nvim-tree/nvim-web-devicons",
   },
   config = function()
     require("lualine").setup({
       options = {
-          theme = "auto"
-      }
+        theme = "auto",
+        component_separators = { left = '', right = '' },
+        section_separators = { left = '', right = '' },
+      },
     })
-  end
+  end,
 }
--- nvim/lua/remaps.lua
+++ nvim/lua/remaps.lua
@@ -2,7 +2,7 @@ vim.g.mapleader = " "
 
 vim.keymap.set("n", "<leader>x", ":bd<CR>", {desc="Close buffer"})
 
-vim.keymap.set("n", "<leader>P", ":Explore<CR>", {desc="Open netrw"})
+--vim.keymap.set("n", "<leader>P", ":Explore<CR>", {desc="Open netrw"})
 
 vim.keymap.set("n", "<leader>OO", ":ObsidianOpen<CR>", {desc="Open in Obsidian"})
 vim.keymap.set("n", "<leader>ON", ":ObsidianNew<CR>", {desc="New md file in Obsidian vault"})
@@ -14,3 +14,7 @@ vim.keymap.set("n", "<C-j>", "<cmd> TmuxNavigateDown<CR>", {})
 vim.keymap.set("n", "<C-k>", "<cmd> TmuxNavigateUp<CR>", {})
 
 vim.keymap.set("n", "<leader>F", vim.lsp.buf.format, {desc="Format current file"})
+
+vim.keymap.set("n", "<C-n>", ":Neotree filesystem toggle left<CR>", {desc="Open explorer left"})
+vim.keymap.set("n", "<leader>P", ":Neotree filesystem toggle current<CR>", {desc="Open explorer"})
+
