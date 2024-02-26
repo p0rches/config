dec24c7d70c2515d52d59639eeb3114e91f31acd
Alex <ascott.sco@gmail.com>
2024-02-26 21:08:13 +0000

    Removed deprecated function calls from obsidian.nvim, added sql to treesitter

--- nvim/lua/config/keymaps.lua
+++ nvim/lua/config/keymaps.lua
@@ -1,3 +1,8 @@
 -- Keymaps are automatically loaded on the VeryLazy event
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
+
+-- Obsidian
+vim.keymap.set("n", "<leader>OO", ":ObsidianOpen<CR>", { desc = "Open in Obsidian" })
+vim.keymap.set("n", "<leader>ON", ":ObsidianNew<CR>", { desc = "New md file in Obsidian vault" })
+vim.keymap.set("n", "<leader>OT", ":ObsidianTemplate", { desc = "Insert Obsidian template" })
