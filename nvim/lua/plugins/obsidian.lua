34ef06eb6f105f54af9eca39ab72efbf34cf9bfa
Alex <ascott.sco@gmail.com>
2024-05-18 13:04:49 +0100

    Update neovim to v0.10.+, config changes for obsidian.nvim

--- nvim/lua/plugins/obsidian.lua
+++ nvim/lua/plugins/obsidian.lua
@@ -49,7 +49,7 @@ return {
       -- Either 'wiki' or 'markdown'.
       preferred_link_style = "wiki",
 
-      prepend_note_id = true,
+      prepend_note_id = false,
 
       -- Optional, configure key mappings. These are the defaults. If you don't want to set any keymappings this
       -- way then set 'mappings = {}'.
@@ -71,22 +71,23 @@ return {
       },
 
       -- Optional, customize how names/IDs for new notes are created.
-      --note_id_func = function(title)
-      --	-- Create note IDs in a Zettelkasten format with a timestamp and a suffix.
-      --	-- In this case a note with the title 'My new note' will be given an ID that looks
-      --	-- like '1657296016-my-new-note', and therefore the file name '1657296016-my-new-note.md'
-      --	local suffix = ""
-      --	if title ~= nil then
-      --		-- If title is given, transform it into valid file name.
-      --		suffix = title:gsub(" ", "-"):gsub("[^A-Za-z0-9-]", ""):lower()
-      --	else
-      --		-- If title is nil, just add 4 random uppercase letters to the suffix.
-      --		for _ = 1, 4 do
-      --			suffix = suffix .. string.char(math.random(65, 90))
-      --		end
-      --	end
-      --	return tostring(os.time()) .. "-" .. suffix
-      --end,
+      note_id_func = function(title)
+        -- Create note IDs in a Zettelkasten format with a timestamp and a suffix.
+        -- In this case a note with the title 'My new note' will be given an ID that looks
+        -- like '1657296016-my-new-note', and therefore the file name '1657296016-my-new-note.md'
+        local suffix = ""
+        if title ~= nil then
+          -- If title is given, transform it into valid file name.
+          suffix = title:gsub(" ", "-"):gsub("[^A-Za-z0-9-]", ""):lower()
+        else
+          -- If title is nil, just add 4 random uppercase letters to the suffix.
+          for _ = 1, 4 do
+            suffix = suffix .. string.char(math.random(65, 90))
+          end
+        end
+        --return tostring(os.time()) .. "-" .. suffix
+        return suffix
+      end,
 
       -- Optional, customize the default name or prefix when pasting images via `:ObsidianPasteImg`.
       image_name_func = function()
