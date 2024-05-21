f9d61ade6189d0a51ef2990be367e6279d8da485
Alex <ascott.sco@gmail.com>
2024-05-21 11:40:39 +0100

    Configured untitled-ai/jupyter_ascending.vim, including keymaps

--- nvim/lua/config/keymaps.lua
+++ nvim/lua/config/keymaps.lua
@@ -6,3 +6,8 @@
 vim.keymap.set("n", "<leader>OO", ":ObsidianOpen<CR>", { desc = "Open in Obsidian" })
 vim.keymap.set("n", "<leader>ON", ":ObsidianNew<CR>", { desc = "New md file in Obsidian vault" })
 vim.keymap.set("n", "<leader>OT", ":ObsidianTemplate", { desc = "Insert Obsidian template" })
+
+-- Jupyter_ascending
+vim.keymap.set("n", "<leader>JE", ":JupyterExecute<CR>", { desc = "Execute current Jupyter cell" })
+vim.keymap.set("n", "<leader>JA", ":JupyterExecuteAll<CR>", { desc = "Execute all Jupyter cells" })
+vim.keymap.set("n", "<leader>JR", ":JupyterRestart<CR>", { desc = "Restart the Jupyter kernel" })
