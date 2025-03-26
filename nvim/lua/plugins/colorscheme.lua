841a4044aaa9be4cbacaca7c427990f2e4dca283
Alex <ascott.sco@gmail.com>
2025-03-26 12:38:12 +0000

    Fixed alacritty cfg

--- nvim/lua/plugins/colorscheme.lua
+++ nvim/lua/plugins/colorscheme.lua
@@ -1,4 +1,14 @@
 return {
 	--"aliqyan-21/darkvoid.nvim",
-  "ellisonleao/gruvbox.nvim"
+  "ellisonleao/gruvbox.nvim",
+  config = function ()
+      require("gruvbox").setup({
+          dim_inactive = false,
+          transparent_mode = false,
+          palette_overrides = {
+              black = "#282828",
+          }
+      })
+      vim.cmd("colorscheme gruvbox")
+  end
 }
