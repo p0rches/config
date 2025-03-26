0e5aee0eaf99d32aa1ddea936b1ee435b332553b
Alex <ascott.sco@gmail.com>
2025-03-26 13:49:03 +0000

    Added proper on_attach behaviour and bindings for lsp. Removed nvim/.git reference.

--- nvim/lua/plugins/lsp.lua
+++ nvim/lua/plugins/lsp.lua
@@ -14,12 +14,31 @@ return {
             }
         },
         config = function()
-            local capabilities = require("blink.cmp").get_lsp_capabilities()
             local lspconfig = require("lspconfig")
+            local capabilities = require("blink.cmp").get_lsp_capabilities()
+            local on_attach = function(client, bufnr)
+                local opts = { buffer = bufnr, remap = false }
 
-            lspconfig["lua_ls"].setup({ capabilities = capabilities })
-            lspconfig["marksman"].setup({ capabilities = capabilities })
-            lspconfig["clangd"].setup({ capabilities = capabilities })
+                vim.keymap.set("n", "gd", function() vim.lsp.buf.definition() end, opts)
+                vim.keymap.set("n", "K", function() vim.lsp.buf.hover() end, opts)
+                vim.keymap.set("n", "<leader>vws", function() vim.lsp.buf.workspace_symbol() end, opts)
+                vim.keymap.set("n", "<leader>vd", function() vim.diagnostic.open_float() end, opts)
+                vim.keymap.set("n", "[d", function() vim.diagnostic.goto_next() end, opts)
+                vim.keymap.set("n", "]d", function() vim.diagnostic.goto_next() end, opts)
+                vim.keymap.set("n", "<leader>vca", function() vim.lsp.buf.code_action() end, opts)
+                vim.keymap.set("n", "<leader>vrr", function() vim.lsp.buf.references() end, opts)
+                vim.keymap.set("n", "<leader>vrn", function() vim.lsp.buf.rename() end, opts)
+            end
+
+            local options = {
+                capabilities = capabilities,
+                on_attach = on_attach
+            }
+
+
+            lspconfig["lua_ls"].setup( options )
+            lspconfig["marksman"].setup( options )
+            lspconfig["clangd"].setup( options )
         end,
     }
 }
