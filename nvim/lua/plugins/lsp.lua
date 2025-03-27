7d3bfc9eabfe6068933dccd4ada3830da67b4280
Alex <ascott.sco@gmail.com>
2025-03-27 08:18:25 +0000

    Fixed keymaps: options and typo corrected

--- nvim/lua/plugins/lsp.lua
+++ nvim/lua/plugins/lsp.lua
@@ -17,17 +17,16 @@ return {
             local lspconfig = require("lspconfig")
             local capabilities = require("blink.cmp").get_lsp_capabilities()
             local on_attach = function(client, bufnr)
-                local opts = { buffer = bufnr, remap = false }
 
-                vim.keymap.set("n", "gd", function() vim.lsp.buf.definition() end, opts)
-                vim.keymap.set("n", "K", function() vim.lsp.buf.hover() end, opts)
-                vim.keymap.set("n", "<leader>vws", function() vim.lsp.buf.workspace_symbol() end, opts)
-                vim.keymap.set("n", "<leader>vd", function() vim.diagnostic.open_float() end, opts)
-                vim.keymap.set("n", "[d", function() vim.diagnostic.goto_next() end, opts)
-                vim.keymap.set("n", "]d", function() vim.diagnostic.goto_next() end, opts)
-                vim.keymap.set("n", "<leader>vca", function() vim.lsp.buf.code_action() end, opts)
-                vim.keymap.set("n", "<leader>vrr", function() vim.lsp.buf.references() end, opts)
-                vim.keymap.set("n", "<leader>vrn", function() vim.lsp.buf.rename() end, opts)
+                vim.keymap.set("n", "gd", function() vim.lsp.buf.definition() end, { desc = "Go to definiton", buffer = bufnr, remap = false })
+                vim.keymap.set("n", "K", function() vim.lsp.buf.hover() end, { desc = "Display hover information", buffer = bufnr, remap = false })
+                vim.keymap.set("n", "<leader>vws", function() vim.lsp.buf.workspace_symbol() end, { desc = "List symbols in the current workspace", buffer = bufnr, remap = false })
+                vim.keymap.set("n", "<leader>vd", function() vim.diagnostic.open_float() end, { desc = "Show diagnostics in a floating window", buffer = bufnr, remap = false })
+                vim.keymap.set("n", "[d", function() vim.diagnostic.goto_next() end, { desc = "Go to next diagnostic", buffer = bufnr, remap = false })
+                vim.keymap.set("n", "]d", function() vim.diagnostic.goto_prev() end, { desc = "Go to previous diagnostic", buffer = bufnr, remap = false })
+                vim.keymap.set("n", "<leader>vca", function() vim.lsp.buf.code_action() end, { desc = "Open available code actions", buffer = bufnr, remap = false })
+                vim.keymap.set("n", "<leader>vrr", function() vim.lsp.buf.references() end, { desc = "List references to the current symbol", buffer = bufnr, remap = false })
+                vim.keymap.set("n", "<leader>vrn", function() vim.lsp.buf.rename() end, { desc = "Rename the current file", buffer = bufnr, remap = false })
             end
 
             local options = {
