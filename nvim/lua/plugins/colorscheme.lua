82aaee80b8d6569dd52adb0f73bd59bc8632bc65
Alex <ascott.sco@gmail.com>
2025-04-15 15:17:36 +0100

    Changed to a nordic theme

--- nvim/lua/plugins/colorscheme.lua
+++ nvim/lua/plugins/colorscheme.lua
@@ -1,14 +1,25 @@
 return {
 	--"aliqyan-21/darkvoid.nvim",
-  "ellisonleao/gruvbox.nvim",
-  config = function ()
-      require("gruvbox").setup({
-          dim_inactive = false,
-          transparent_mode = true,
-          palette_overrides = {
-              black = "#282828",
-          }
-      })
-      vim.cmd("colorscheme gruvbox")
-  end
+    --"ellisonleao/gruvbox.nvim",
+    --config = function ()
+    --    require("gruvbox").setup({
+    --        dim_inactive = false,
+    --        transparent_mode = true,
+    --        palette_overrides = {
+    --            black = "#282828",
+    --        }
+    --    })
+    --    vim.cmd("colorscheme gruvbox")
+    --end
+    "AlexvZyl/nordic.nvim",
+    lazy = false,
+    priority = 1000,
+    config = function()
+        require("nordic").setup({
+            transparent = {
+                bg = true,
+            }
+        })
+        require("nordic").load()
+    end
 }
